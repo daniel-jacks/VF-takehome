d6dbcdb0020e7029641df2196edc7b65b89da5e6
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-04-11 15:30:41 -0500

    finishes writing test file for app.js, and makes some final changes to app.js

--- vanity-generator/function/app.js
+++ vanity-generator/function/app.js
@@ -1,21 +1,8 @@
 //-- Bring in the aws-sdk, and used the DocumentClient constructor --//
 //-- The Document Client acts as an interpreter between JavaScript and DynamoDB --//
 const AWS = require('aws-sdk');
-AWS.config.update({ region: "us-west-2" });
+AWS.config.update({ region: 'us-west-2' });
 const ddb = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' });
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 
 //-- Here is the word list we use to check if the strings returned from our function are actual words --//
 const fs = require('fs');
@@ -35,11 +22,11 @@ exports.lambdaHandler = async (event, context, callback) => {
 
     //-- Checking whether or not the number is valid, and if not immediately ends the Lambda and returns error status code and error message --//
     if (!verifyNumber(phoneNumber)) {
-        console.log('unsupported');
         callback(null, {
             statusCode: 400,
             body: 'Unsupported phone number',
         })
+        return 'unsupported phone number';
     }
 
     //-- Parallel arrays used to match the number to it's corresponding letters --//
@@ -49,7 +36,7 @@ exports.lambdaHandler = async (event, context, callback) => {
     //-- A set used to store vanity numbers that are generated, and ultimately stored in DynamoDB --//
     let vanityNumbers = new Set();
 
-    //-- Counter used for testing purposes, which will count how many times the 'walk' function is run --//
+    //-- Counter used for testing purposes, which will count how many times the 'step' function is run --//
     let counter = 0;
 
     //-- Helper function used to map numbers to options --//
@@ -59,56 +46,54 @@ exports.lambdaHandler = async (event, context, callback) => {
     }
 
     //-- Recursive function used to calculate the words you are able to create with a given phone number --//
-    function walk(string, tracker, output = '', index = 0) {
-        counter++; // increments counter to keep track of how many times 'walk' has been invoked
-        if (string[index]) { // base case, once the index becomes greater than the word length, 'walk' stops being called
+    function step(string, tracker, output = '', index = 0) {
+        counter++; // increments counter to keep track of how many times 'step' has been invoked
+        if (string[index]) { // base case, once the index becomes greater than the word length, 'step' stops being called
             let letters = indexFinder(string[index]); // gets the letters associated to the number at index from string input
             letters?.forEach(lett => { // used to iterate over each letter from option and perform an action
-                let strAdd = output; // 'strAdd' and 'nextIdx' needed to have a more narrowed scope, because 'walk' is essentially being called on 3 different k-ary trees
+                let strAdd = output; // 'strAdd' and 'nextIdx' needed to have a more narrowed scope, because 'step' is essentially being called on 3 different k-ary trees
                 let nextIdx = index + 1;
                 strAdd += lett; // concatenates the string with the next possible letter
-                if (wordList.includes(strAdd) && (strAdd.length + tracker === (phoneNumber.length + 1))) {
+                if (wordList.includes(strAdd) && (strAdd.length + (tracker - 1) === (phoneNumber.length))) {
                     vanityNumbers.add(`${phoneNumber.substring(0, (tracker - 1))}${strAdd}`);
-                } 
+                };
                 let regex = new RegExp(`^${strAdd}.*`, 'g');
-                if (vanityNumbers.size < 5 && wordList.find(word => word.match(regex))) walk(string, tracker, strAdd, nextIdx);
+                if (vanityNumbers.size < 5 && wordList.find(word => word.match(regex))) step(string, tracker, strAdd, nextIdx);
             })
         }
     }
 
-    //-- The starting substring for our 'walk' function, which begins at 5, making our input the last 7 digits from the input phone number --//
+    //-- The starting substring for our 'step' function, which begins at 5, making our input the last 7 digits from the input phone number --//
     let stringStart = 5;
     let input = event.Details.ContactData.CustomerEndpoint.Address.substring(stringStart);
 
     //-- If vanity numbers have previously been calculated for our input phone number, Lambda will just return those options --//
     let data = await getFromDB(phoneNumber);
 
-    if (Object.keys(data).length < 1) {
-        //-- Iteratively call the walk function, with the input progressively getting shorter --//
-        while (input.length >= 4) {
-            walk(input, stringStart);
+    if (Object.keys(data)?.length < 1) {
+        //-- Iteratively call the step function, with the input progressively getting shorter --//
+        let minVanityLength = 4; // Potentially ask a caller for input and ask for their minimum length vanity word
+        while (input.length >= minVanityLength) {
+            step(input, stringStart);
             input = event.Details.ContactData.CustomerEndpoint.Address.substring(stringStart++);
-        }
+        };
 
         //-- Join the 'vanityNumber' Set to be a string separated by commas --//
         let result = [...vanityNumbers].join(', ');
-        console.log(counter);
 
-        await addToDB(phoneNumber, result)
-            .then(() => {
-                callback(null, {
-                    statusCode: 201,
-                    body: result,
-                })
-            })
-            .catch((err) => {
-                console.log(err)
-            });
-    } else {
+        await addToDB(phoneNumber, result);
         callback(null, {
             statusCode: 201,
             body: result,
         })
+        return result;
+
+    } else if (!Object.keys(data).length < 1) {
+        callback(null, {
+            statusCode: 201,
+            body: data?.Item?.vanityNumbers,
+        });
+        return data.Item.vanityNumbers;
     }
 };
 
@@ -116,7 +101,7 @@ exports.lambdaHandler = async (event, context, callback) => {
 function addToDB(requestId, data) {
     console.log('Add to DB called');
     const params = {
-        TableName: "VanityNumbers",
+        TableName: 'VanityNumbers',
         Item: {
             'phoneNumber': requestId,
             'vanityNumbers': data,
@@ -129,7 +114,7 @@ function addToDB(requestId, data) {
 function getFromDB(requestId) {
     console.log('Get from DB called');
     const params = {
-        TableName: "VanityNumbers",
+        TableName: 'VanityNumbers',
         Key: {
             'phoneNumber': requestId,
         }
--- vanity-generator/function/tests/unit/test-handler.js
+++ vanity-generator/function/tests/unit/test-handler.js
@@ -1,59 +1,151 @@
 'use strict';
 
 const app = require('../../app.js');
-const chai = require('chai');
-const AWSMock = require('aws-sdk-mock');
-const expect = chai.expect;
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const { DynamoDB } = require('aws-sdk');
 let event, context;
 
-describe('Tests vanity generator', function () {
-    console.log('here', AWSMock);
+const sandbox = sinon.createSandbox();
 
-    it('verifies successful response', async () => {
+describe('Tests vanity generator', function () {
+    const log = sinon.spy(console, 'log');
+
+    it('verifies successful response if phone number already exists in database', async () => {
+        let data = { Item: { vanityNumbers: '+1800testing, +1800verting, +1800vesting, +180083ruing, +180083sting' } };
+        sandbox.stub(DynamoDB.DocumentClient.prototype, 'get').returns({ promise: () => data });
+        const callback = sinon.spy();
+
         event = {
-            Name: "ContactFlowEvent",
+            Name: 'ContactFlowEvent',
             Details: {
                 ContactData: {
                     Attributes: {},
-                    Channel: "VOICE",
-                    ContactId: "5ca32fbd-8f92-46af-92a5-6b0f970f0efe",
+                    Channel: 'VOICE',
+                    ContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
                     CustomerEndpoint: {
-                        Address: "+18006228464",
-                        Type: "TELEPHONE_NUMBER"
+                        Address: '+18008378464',
+                        Type: 'TELEPHONE_NUMBER'
                     },
-                    InitialContactId: "5ca32fbd-8f92-46af-92a5-6b0f970f0efe",
-                    InitiationMethod: "API",
-                    InstanceARN: "arn:aws:connect:us-east-1:123456789012:instance/9308c2a1-9bc6-4cea-8290-6c0b4a6d38fa",
+                    InitialContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    InitiationMethod: 'API',
+                    InstanceARN: 'arn:aws:connect:us-east-1:123456789012:instance/9308c2a1-9bc6-4cea-8290-6c0b4a6d38fa',
                     MediaStreams: {
                         Customer: {
                             Audio: {
-                                StartFragmentNumber: "91343852333181432392682062622220590765191907586",
-                                StartTimestamp: "1565781909613",
-                                StreamARN: "arn:aws:kinesisvideo:us-east-1:123456789012:stream/connect-contact-a3d73b84-ce0e-479a-a9dc-5637c9d30ac9/1565272947806"
+                                StartFragmentNumber: '91343852333181432392682062622220590765191907586',
+                                StartTimestamp: '1565781909613',
+                                StreamARN: 'arn:aws:kinesisvideo:us-east-1:123456789012:stream/connect-contact-a3d73b84-ce0e-479a-a9dc-5637c9d30ac9/1565272947806'
                             }
                         }
                     },
-                    PreviousContactId: "5ca32fbd-8f92-46af-92a5-6b0f970f0efe",
+                    PreviousContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
                     Queue: null,
                     SystemEndpoint: {
-                        Address: "+11234567890",
-                        Type: "TELEPHONE_NUMBER"
+                        Address: '+11234567890',
+                        Type: 'TELEPHONE_NUMBER'
                     }
                 },
                 Parameters: {}
             }
         }
 
-        const result = await app.lambdaHandler(event, context)
+        const result = await app.lambdaHandler(event, context, callback);
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+        expect(result).to.be.an('string');
+        expect(result).to.equal('+1800testing, +1800verting, +1800vesting, +180083ruing, +180083sting');
+        expect(callback.callCount).to.equal(1);
+        expect(log.callCount).to.equal(1);
+    });
 
-        let response = JSON.parse(result.body);
+    it('verifies successful response if phone number does not already exist in database', async () => {
+        sandbox.restore();
+        let data = { result: '+1800testing, +1800verting, +1800vesting, +180083ruing, +180083sting' };
+        sandbox.stub(DynamoDB.DocumentClient.prototype, 'put').returns({ promise: () => data });
+        const callback = sinon.spy();
 
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal('0 luck, 1 ta, 1 taj, 1 tak, 2 al');
-        // expect(response.location).to.be.an("string");
+        event = {
+            Name: 'ContactFlowEvent',
+            Details: {
+                ContactData: {
+                    Attributes: {},
+                    Channel: 'VOICE',
+                    ContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    CustomerEndpoint: {
+                        Address: '+18008378464',
+                        Type: 'TELEPHONE_NUMBER'
+                    },
+                    InitialContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    InitiationMethod: 'API',
+                    InstanceARN: 'arn:aws:connect:us-east-1:123456789012:instance/9308c2a1-9bc6-4cea-8290-6c0b4a6d38fa',
+                    MediaStreams: {
+                        Customer: {
+                            Audio: {
+                                StartFragmentNumber: '91343852333181432392682062622220590765191907586',
+                                StartTimestamp: '1565781909613',
+                                StreamARN: 'arn:aws:kinesisvideo:us-east-1:123456789012:stream/connect-contact-a3d73b84-ce0e-479a-a9dc-5637c9d30ac9/1565272947806'
+                            }
+                        }
+                    },
+                    PreviousContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    Queue: null,
+                    SystemEndpoint: {
+                        Address: '+11234567890',
+                        Type: 'TELEPHONE_NUMBER'
+                    }
+                },
+                Parameters: {}
+            }
+        }
+
+        const result = await app.lambdaHandler(event, context, callback);
+        expect(result).to.be.an('string');
+        expect(result).to.equal('+1800testing, +1800verting, +1800vesting, +180083ruing, +180083sting');
+        expect(callback.callCount).to.equal(1);
+        expect(log.callCount).to.equal(3);
+    });
+
+    it('test verifyNumber will return error message if phone number is blank or unsupported', async () => {
+        sandbox.restore();
+        const callback = sinon.spy();
+
+        event = {
+            Name: 'ContactFlowEvent',
+            Details: {
+                ContactData: {
+                    Attributes: {},
+                    Channel: 'VOICE',
+                    ContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    CustomerEndpoint: {
+                        Address: '+1800622846', // This phone number is only 10 characters long, and is supposed to be 11
+                        Type: 'TELEPHONE_NUMBER'
+                    },
+                    InitialContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    InitiationMethod: 'API',
+                    InstanceARN: 'arn:aws:connect:us-east-1:123456789012:instance/9308c2a1-9bc6-4cea-8290-6c0b4a6d38fa',
+                    MediaStreams: {
+                        Customer: {
+                            Audio: {
+                                StartFragmentNumber: '91343852333181432392682062622220590765191907586',
+                                StartTimestamp: '1565781909613',
+                                StreamARN: 'arn:aws:kinesisvideo:us-east-1:123456789012:stream/connect-contact-a3d73b84-ce0e-479a-a9dc-5637c9d30ac9/1565272947806'
+                            }
+                        }
+                    },
+                    PreviousContactId: '5ca32fbd-8f92-46af-92a5-6b0f970f0efe',
+                    Queue: null,
+                    SystemEndpoint: {
+                        Address: '+11234567890',
+                        Type: 'TELEPHONE_NUMBER'
+                    }
+                },
+                Parameters: {}
+            }
+        }
+
+        const result = await app.lambdaHandler(event, context, callback);
+        expect(result).to.be.an('string');
+        expect(result).to.equal('unsupported phone number');
+        expect(callback.callCount).to.equal(1);
     });
 });
